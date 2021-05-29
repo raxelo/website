6f2cbee312988bf93b58f0aeafbeea4d8a8d22d8
pragmare <29938718+pragmare@users.noreply.github.com>
2021-05-28 21:31:37 -0300

    Add some lorem text

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,1 +1,75 @@
-projects
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
+<p class="py-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nostrum eum odit maiores
+	asperiores necessitatibus laboriosam ipsa beatae in minima quod illo, odio temporibus itaque
+	placeat eveniet quas dolorum dolores?
+</p>
